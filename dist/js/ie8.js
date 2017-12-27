f895e9fc9655e3ba9fdf2a41e5c72883419b59e7
18320867985 <huangqishanyx@163.com>
2017-12-27 19:06:02 +0800

    2017-12-27

---
+++ dist/js/ie8.js
@@ -0,0 +1,27 @@
+
+//placeholder  兼容ie8 ie9
+alert("你的浏览器版本过低,为了正常使用,请升级你的浏览器。");
+//$(function(){
+//	
+//	$("input[type='text'],input[type='password']").each(function(){
+//	var place=$(this).attr("placeholder");
+//	$(this).val(place);
+//});
+//$("input[type='text'],input[type='password'").focus(function(){
+//	var place=$(this).attr("placeholder");
+//	var val=$(this).val();
+//	if(place==val){
+//	$(this).val("");
+//	}
+//});
+//$("input[type='text'],input[type='password'").blur(function(){
+//	var place=$(this).attr("placeholder");
+//	if($(this).val()==""){
+//		$(this).val(place);
+//	}
+//});
+//
+//});
+//
+
+
