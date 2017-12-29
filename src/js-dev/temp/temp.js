241aa41954ee1a203d573da20c7850ee1755cb7c
18320867985 <huangqishanyx@163.com>
2017-12-29 17:54:48 +0800

    2017-12-29

--- src/js-dev/temp/temp.js
+++ src/js-dev/temp/temp.js
@@ -448,7 +448,34 @@ var threeAddress = function () {
 		$(this).trigger("radio_click", [this]);
 	});
 }(window.jQuery || window.Zepto);
-
+//  table 鼠标hover样式tooltip
+//
+//$("table").on("mouseenter", ".hover-tooltip", function(e) {
+//	$(this).css("text-align", "center");
+//	var div = document.createElement("div");
+//	div.className = "tooltip-cont";
+//	div.innerText = $(this).text();
+//	div.style.border = "1px solid #ddd";
+//	div.style.borderRadius = "5px";
+//	div.style.position = "absolute";
+//	//div.style.marginLeft=,$(this).outerWidth()/2+"px";		
+//	div.style.background = "#fff";
+//	div.style.padding = "3px 10px ";
+//	div.style.lineHeight = "30px";
+//	div.style.boxShadow = "2px 2px 5px #ddd,-2px 0px 5px #ddd";
+//	div.style.zIndex = "99999";
+//	div.style.width = "auto";
+//	
+//	$(this).append(div);
+//	
+//	return false;
+//
+//});
+//
+//$("table").on("mouseleave", ".hover-tooltip", function() {
+//
+//	$(this).find(".tooltip-cont").remove();
+//});
 /*
   
 <div class="number" >
