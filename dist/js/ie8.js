be97c51c405fd1239f81486178021f045c937344
18320867985 <huangqishanyx@163.com>
2018-01-06 12:13:33 +0800

    2018-1-6

--- dist/js/ie8.js
+++ dist/js/ie8.js
@@ -1,6 +1,6 @@
 
 //placeholder  兼容ie8 ie9
-//alert("你的浏览器版本过低,为了正常使用,请升级你的浏览器。");
+alert("你的浏览器版本过低,为了正常使用,请升级你的浏览器。");
 //$(function(){
 //	
 //	$("input[type='text'],input[type='password']").each(function(){
